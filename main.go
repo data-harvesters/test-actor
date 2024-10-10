43307daeceb7fb626008181b707a5197b3092a69
skateboard <withdraws@gmail.com>
2024-10-10 11:13:21 +0400

    some light testing

--- main.go
+++ main.go
@@ -1,8 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/data-harvesters/goapify"
+)
+
+type testInput struct {
+	Input string `json:"input"`
+}
 
 func main() {
 	fmt.Println("HELLO FROM TEST ACTOR")
 
+	a := goapify.NewActor(os.Getenv("APIFY_DEFAULT_KEY_VALUE_STORE_ID"),
+		os.Getenv("APIFY_TOKEN"),
+		os.Getenv("APIFY_DEFAULT_DATASET_ID"))
+
+	input := new(testInput)
+
+	err := a.Input(input)
+	if err != nil {
+		panic(err)
+
+	}
+
+	fmt.Println(input.Input)
 }
